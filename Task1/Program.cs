8fcb87fccb2e5790e69a8ddae0b9491a62bc7783
Tatiana <ttt-ttt@bk.ru>
2022-07-06 00:00:54 +0300

    search for the number of the coordinate quarter by the coordinates of the point

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача - на вход подается точка с координатами (х, у), причем х, у не равны нулю.
+// Определить в какой координатной четверти лежит точка
+Console.WriteLine("Введите координату х");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y");
+double y = Convert.ToDouble(Console.ReadLine());
+FindCoordinateQuarter(x, y);
+
+
+void FindCoordinateQuarter(double abscissa, double ordinate)
+{
+    if (abscissa == 0 || ordinate == 0)
+        Console.WriteLine("Точка лежит на координатной прямой.");
+    else if (abscissa > 0 && ordinate > 0)
+        Console.WriteLine("Точка находится в первой четверти.");
+    else if (abscissa > 0 && ordinate < 0)
+        Console.WriteLine("Точка находится во второй четверти.");
+    else if (abscissa < 0 && ordinate > 0)
+        Console.WriteLine("Точка находится в четвертой четверти.");
+    else
+        Console.WriteLine("Точка находится в третьей четверти.");
+}
